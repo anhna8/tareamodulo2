7cc1a01340e9240619a9a9d8ab6109863790c4c5
anhna <ana_mjl@hotmail.com>
2025-02-17 13:20:08 -0600

    first commit

---
+++ tarea3/main.js
@@ -0,0 +1,27 @@
+/* const frutas = ["manzana", "pera", "sandia", "fresa", "kiwi"];
+
+const cantidadFrutas = {
+    manzana: 5,
+    pera: 3,
+    kiwi:10,
+    sandia:1,
+    fresa: 15
+}
+
+for(let i = 0; i < frutas.length; i++){
+    console.log(frutas[i]);
+    console.log(cantidadFrutas[frutas[i]]);
+} */
+
+let i = 0;
+
+while(i < frutas.length){
+    const frutas = frutas[i];
+    if (cantidadFrutas[fruta]){
+        cantidadFrutas[frutas]++;
+    } else {
+        cantidadFrutas[frutas] = 1;
+    }
+}
+
+console.log("cantidad de cada tipo de fruta: ", cantidadFrutas);
