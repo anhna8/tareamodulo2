21833426b56b7f8f44b373fd0b959c59526e9726
anhna <ana_mjl@hotmail.com>
2025-02-19 07:04:43 -0600

    primer commit

---
+++ tarea4/main.js
@@ -0,0 +1,26 @@
+/*Define una función `agregarLibro(titulo)`, que añada un libro a un array llamado `librosLeidos`.
+Define una función `mostrarLibrosLeidos()`, que imprima todos los libros que has leído.*/ 
+
+let librosLeidos = [];
+
+function agregarLibro(titulo) {
+    librosLeidos.push(titulo);
+}
+
+function mostrarLibrosLeidos() {
+    console.log("Libros leídos:");
+    if (librosLeidos.length === 0) {
+        console.log("No has leído ningún libro todavía.");
+    } else {
+        librosLeidos.forEach(function(libro, index) {
+            console.log(`${index + 1}. ${libro}`);
+        });
+    }
+}
+
+agregarLibro("harry potter");
+agregarLibro("el principito");
+agregarLibro("la divina comedia");
+agregarLibro("la historia del loco");
+agregarLibro("los hornos de hitler");
+mostrarLibrosLeidos();
